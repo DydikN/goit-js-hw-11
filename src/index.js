4f600677a1079c1cb571e01375efb6d2c6f1f7f8
DydikN <PingvinPain@gmail.com>
2022-12-11 19:00:37 +0200

    done hw

--- src/index.js
+++ src/index.js
@@ -1,105 +1,94 @@
 import './css/styles.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchImages } from './js/fetch-images';
+import { renderGallery } from './js/render-gallery';
+import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { fetchImages } from './js/fetchImg';
-const DEBOUNCE_DELAY = 300;
-let q = '';
+
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.btn-load-more');
+let query = '';
 let page = 1;
 let simpleLightBox;
 const perPage = 40;
-const refs = {
-  input: document.querySelector('.input'),
-  form: document.querySelector('#search-form'),
-  div: document.querySelector('.gallery'),
-  button: document.querySelector('.btn-load-more'),
-};
-function plavno() {
-  const { height: cardHeight } = document
-    .querySelector('.gallery')
-    .firstElementChild.getBoundingClientRect();
 
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
-  });
-}
-function createGalleryMarkup(data) {
-  return data.reduce(
-    (
-      acc,
-      { webformatURL, largeImageURL, tags, likes, views, comments, downloads }
-    ) => `${acc}<div class="photo-card">
-  <a class="gallery__item" href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" hight = 620 width = 420/></a>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b><p>${likes}</p>
-    </p>
-    <p class="info-item">
-      <b>Views</b><p>${views}</p>
-    </p>
-    <p class="info-item">
-      <b>Comments</b><p>${comments}</p>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b><p>${downloads}</p>
-    </p>
-  </div>
-</div>`,
-    ''
-  );
-}
-function onError() {
-  return 0;
-}
-function addMarkup(data) {
-  return refs.div.insertAdjacentHTML('beforeend', createGalleryMarkup(data));
-}
-function onSubmit(e) {
-  page = 1;
+searchForm.addEventListener('submit', onSearchForm);
+loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+
+onScroll();
+
+function onSearchForm(e) {
   e.preventDefault();
-  q = e.currentTarget.searchQuery.value.trim();
-  if (!q) return;
-  refs.div.innerHTML = '';
-  fetchImages(q, page, perPage)
+  window.scrollTo({ top: 0 });
+  page = 1;
+  query = e.currentTarget.searchQuery.value.trim();
+  gallery.innerHTML = '';
+  loadMoreBtn.classList.add('is-hidden');
+
+  if (query === '') {
+    alertNoEmptySearch();
+    return;
+  }
+
+  fetchImages(query, page, perPage)
     .then(({ data }) => {
       if (data.totalHits === 0) {
-        refs.button.classList.add('is-hidden');
-        refs.div.innerHTML = '';
-        return Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
+        alertNoImagesFound();
+      } else {
+        renderGallery(data.hits);
+        simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+        alertImagesFound(data);
+
+        if (data.totalHits > perPage) {
+          loadMoreBtn.classList.remove('is-hidden');
+        }
       }
-      if (data.totalHits > perPage) {
-        refs.button.classList.remove('is-hidden');
-        refs.button.textContent = 'Load More';
-      }
-      if (data.totalHits < perPage && data.totalHits != 0) {
-        refs.button.classList.add('is-hidden');
-      }
-      addMarkup(data.hits);
-      simpleLightBox = new SimpleLightbox('.gallery a').refresh();
-      Notify.success(`Hooray! We found ${data.totalHits} images.`);
     })
-    .catch(onError);
+    .catch(error => console.log(error))
+    .finally(() => {
+      searchForm.reset();
+    });
 }
-function onLoadMore(e) {
+
+function onLoadMoreBtn() {
   page += 1;
   simpleLightBox.destroy();
-  fetchImages(q, page, perPage)
+
+  fetchImages(query, page, perPage)
     .then(({ data }) => {
-      addMarkup(data.hits);
+      renderGallery(data.hits);
       simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+
       const totalPages = Math.ceil(data.totalHits / perPage);
+
       if (page > totalPages) {
-        refs.button.classList.add('is-hidden');
-        return Notify.failure(
-          "We're sorry, but you've reached the end of search results."
-        );
+        loadMoreBtn.classList.add('is-hidden');
+        alertEndOfSearch();
       }
-      plavno();
     })
-    .catch(onError);
+    .catch(error => console.log(error));
 }
-refs.form.addEventListener('submit', onSubmit);
-refs.button.addEventListener('click', onLoadMore);
+
+function alertImagesFound(data) {
+  Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+}
+
+function alertNoEmptySearch() {
+  Notiflix.Notify.failure(
+    'The search string cannot be empty. Please specify your search query.'
+  );
+}
+
+function alertNoImagesFound() {
+  Notiflix.Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
+}
+
+function alertEndOfSearch() {
+  Notiflix.Notify.failure(
+    "We're sorry, but you've reached the end of search results."
+  );
+}
